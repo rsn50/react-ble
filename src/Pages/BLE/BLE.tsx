4ee5c40645d4f5a58e37cc572e2a03b8fdb23120
rsn50 <rohan@innoscent.in>
2024-08-31 15:27:57 +0530

    change width of card container

--- src/Pages/BLE/BLE.tsx
+++ src/Pages/BLE/BLE.tsx
@@ -430,7 +430,7 @@ const BLE: React.FC<IBleProps> = ({
                                 <Button style={{ backgroundColor: "#83BF8D" }} icon={<DownloadOutlined />} type="primary" size={'large'} onClick={saveGraph}>Save Graph</Button>
                             </Space>
 
-                            <div id='chart-container' style={{ width: '80vw' }}>
+                            <div id='chart-container' >
                                 <Space direction="vertical" size="middle" style={{ width: '1000px' }}>
 
                                     <br /><br />
